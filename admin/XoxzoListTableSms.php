908430e630ee3c34a9e6efafc18ac8b6114d6329
Hamza Shahid <hamza@thechakor.com>
2020-03-11 21:16:19 +0500

    Incorrect assumption on directory paths

--- admin/XoxzoListTableSms.php
+++ admin/XoxzoListTableSms.php
@@ -252,9 +252,9 @@ class XoxzoListTableSms extends \WP_List_Table {
                     'section' => sanitize_text_field($_POST['section']),
                     'page' => sanitize_text_field($_POST['page']),
                     'tab' => sanitize_text_field($_POST['tab']),
-                    'paged' => sansanitize_text_fielditize_text_field($_POST['paged']),
+                    'paged' => sanitize_text_field($_POST['paged']),
                 ),
-                admin_url( '/wp-admin/admin.php' )   
+                admin_url( 'admin.php' )   
             );
             wp_redirect(
                 $redirect_url
