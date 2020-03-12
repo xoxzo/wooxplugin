0b78a56f851ba08c011c974c6325259b7e5bd745
Hamza Shahid <hamza@thechakor.com>
2020-03-12 17:32:04 +0500

    File Calling Path Issue Resolved

--- admin/XoxzoListTableSms.php
+++ admin/XoxzoListTableSms.php
@@ -259,7 +259,7 @@ class XoxzoListTableSms extends \WP_List_Table {
                     'tab' => sanitize_text_field($_POST['tab']),
                     'paged' => sansanitize_text_fielditize_text_field($_POST['paged']),
                 ),
-                admin_url( '/wp-admin/admin.php' )   
+                admin_url( 'admin.php' )   
             );
             wp_redirect(
                 $redirect_url
